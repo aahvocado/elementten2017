f734693228d9c9f99d0bd5baf2ddb7896b50372c
neonwednesdays <dxiao.ns@gmail.com>
2017-03-19 19:50:17 -0400

    FloatingButton component; wip to restructure home

---
+++ src/Components/FloatingButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../Styles/floating-button.css';
+
+export default class FloatingButton extends React.Component {
+	static get defaultProps() {
+        return {
+			txt: '',
+			PromiseLinkDidPress: () => {}
+        }
+    }
+
+	render(){
+		const { txt } = this.props;
+
+		return (
+			<button	className='floating-button'
+					onClick={this.props.PromiseLinkDidPress}>
+					
+				{txt}
+			</button>
+		);
+	}
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+/* components */
 import FloatingPlanet from '../Components/FloatingPlanet';
+import FloatingButton from '../Components/FloatingButton';
 
+/* styles */
 import '../Styles/styles.css';
 import '../Styles/home.css';
 
@@ -9,8 +12,18 @@ export default class Home extends React.Component {
 	render(){
 		return (
 			<div className='home-component'>
+				{/* todo: separate component */}
+				<header>
+					<nav role='navigation'>
+						<FloatingButton txt='this is a link'
+							PromiseLinkDidPress={ () => {
+								console.log('it worked');
+							}} />
+					</nav>
+				</header>
 
 				<div className='center-text'>hello world</div>
+
 				<FloatingPlanet />
 			</div>
 		);
