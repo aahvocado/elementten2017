84d93f2af61d6eec2a8a36e051a641944c9c1309
neonwednesdays <dxiao.ns@gmail.com>
2017-03-19 18:44:01 -0400

    edited templates - created custom components and folders

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import {render} from 'react-dom';
+
+import '../Styles/styles.css';
+
+export default class Home extends React.Component {
+	render(){
+		return (
+			<div>hello world</div>
+		);
+	}
+}
