39ac6edcafaad62902fd716fc645d91ece3ae102
neonwednesdays <dxiao.ns@gmail.com>
2017-03-19 19:26:24 -0400

    started home.css and created simple home page mock

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 // import {render} from 'react-dom';
 
 import '../Styles/styles.css';
+import '../Styles/home.css';
 
 export default class Home extends React.Component {
 	render(){
 		return (
-			<div>hello world</div>
+			<div className='home-component'>
+
+				<div className='center-text'>hello world</div>
+				<div className='floating-planet'></div>
+			</div>
 		);
 	}
 }
