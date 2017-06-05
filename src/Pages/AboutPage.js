c8608e7695ef4197fe2604ffd14b7364bc351c20
neonwednesdays <dxiao.ns@gmail.com>
2017-06-05 02:14:00 -0400

    created about page

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -4,6 +4,8 @@ import { CONSTANTS } from '../constants';
 /* pages */
 import Page from '../Pages/Page';
 
+import '../Styles/about-page.css';
+
 /*
 	page for about page
 */
@@ -15,7 +17,24 @@ export default class AboutPage extends Page {
 
 	renderPageInner() {
 		return (
-			<p>Daniel Xiao is a cool cat</p>
+			<div className="">
+
+				<p className="et-about__description">
+					Daniel Xiao graduated from Georgia Institute of Technology in 2014. He received a Bachelors of Science in Computational Media, which is a major much like Computer Science in that it teaches how to program and utilize technology but it also focuses on digital media and how computers have potential to be an expressive and interactive medium.
+				</p>
+
+				<p className="et-about__description">
+					So now he spending his days building games and applications.
+				</p>
+
+				<p className="et-about__description">
+					He is also looking for employment and would love to talk to you! You can reach him at <a href="mailto:dxiao.ns@gmail.com">dxiao.ns@gmail.com</a>.
+				</p>
+
+				<p className="et-about__description">
+					He also volunteers at an animal shelter and has three pet rats. He loves video games and board games.
+				</p>
+			</div>
 		);
 	}
 }
